830273229a00cb3cebf9a3b7eb90a5517c8025f6
rujptw <2609274343@qq.com>
2018-08-29 07:17:47 +0800

    提取css

--- practice/处理css/webpack.config.js
+++ practice/处理css/webpack.config.js
@@ -1,7 +1,8 @@
 //提示：图标相同即相关联
 // 🆘：重要的提示
 let path = require('path')
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //提取为单独的css文件
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'); //压缩css文件
 module.exports = {
     entry: {
         app: "./src/app.js"
@@ -16,18 +17,25 @@ module.exports = {
         rules: [{
             test: /\.styl$/,
             use: [
- 
                 {
-                    /*☝ */
-                    loader: 'style-loader', //2. 将css放入style标签内
+                    loader: MiniCssExtractPlugin.loader,
                     options: {
-                        insertInto: 'html', //插入某个dom中
-                        singleton: true, //是否将css放入一个style标签内
-                        transform: './css.transform.js' //在浏览器环境下对css进行形变
-                    }
-                    //🐷loader:'style-loader/url'//将css文件引入进来 🆘此方法不常用因为引入多个会打包多个文件，引入时会造成多次网络请求
-                    //☝loader:'style-loader/useable'//控制样式是否插入
+                        // you can specify a publicPath here
+                        // by default it use publicPath in webpackOptions.output
+                        publicPath: './dist/'
+                      }
                 },
+                // {使用MiniCssExtractPlugin时，不能用style-loader
+                //     /*☝ */
+                //     loader: 'style-loader', //2. 将css放入style标签内
+                //     options: {
+                //         insertInto: 'html', //插入某个dom中
+                //         singleton: true, //是否将css放入一个style标签内
+                //         transform: './css.transform.js' //在浏览器环境下对css进行形变
+                //     }
+                //     //🐷loader:'style-loader/url'//将css文件引入进来 🆘此方法不常用因为引入多个会打包多个文件，引入时会造成多次网络请求
+                //     //☝loader:'style-loader/useable'//控制样式是否插入
+                // },
                 {
                     /*☝*/
                     loader: 'css-loader', //1. 将css从js中提取出来
@@ -35,9 +43,9 @@ module.exports = {
                     options: {
                         // minimize: true, 压缩效果已经被移除
                         //启用css-modules
-                        modules:true,
+                        modules: true,
                         // 定义webpack处理过的css名
-                        localIdentName:'[path] [name]_[local]_[hash:base64:5]'
+                        localIdentName: '[path] [name]_[local]_[hash:base64:5]'
                     }
                     //🐷 loader:'file-loader' //打包css为一个文件
                 },
@@ -48,5 +56,11 @@ module.exports = {
             ]
 
         }]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].min.css'
+        }),
+        new OptimizeCssAssetsPlugin({})
+    ]
 }
